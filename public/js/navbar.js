c377255edaaf68365918f71ac3f3721eaf23ada2
Suda Nimh <rslepejian@gmail.com>
2021-02-06 15:28:29 -0800

    deleted navar.js

--- public/js/navbar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var login = $("btn-login-dir")
-    var logout = $("btn-logout")
-
-    login.on("click", function (event) {
-        window.location.replace("/login")
-    })
-    
-});
