cd026260afc60434cd59d58233d856a12d63b06b
Suda Nimh <rslepejian@gmail.com>
2021-02-08 11:27:34 -0800

    removed character association with creature, no longer has killed by

--- models/character.js
+++ models/character.js
@@ -25,17 +25,8 @@ module.exports = function (sequelize, DataTypes) {
         }
     });
 
-    // each character has one creature that killed them, it starts out as null and updates when the character dies
     Character.associate = function (models) {
         Character.belongsTo(models.Location, {
-            foreignKey: {
-                defaultValue: 1
-            }
-        });
-    };
-
-    Character.associate = function (models) {
-        Character.belongsTo(models.Creature, {
             foreignKey: {
                 defaultValue: null
             }
